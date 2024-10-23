012bf5c44bcc73cdb459ee2b73e0b41ec5562e9d
sarfudeen727262 <ahmad.sarfudeen@gmail.com>
2024-10-23 16:59:48 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,22 @@
 package com.driver;
+
 class Product{
   public int product(int x, int y) {
-    return System.out.println(x*y);
+    return x*y;
   }
   public int product(int x, int y, int z) {
-     return System.out.println(x*y*z);
+    return x*y*z;
   }
   public double product(double x, double y) {
-     return System.out.println(x*y);
+    return x*y;
   }
 }
+
 public class Main {
-public static void main (String[] args){
-  Product p =new Product();
-  p.product(2,3);
-   p.product(2,3,4);
-   p.product(3.45,44.4);
-}
-  
+  public static void main (String[] args){
+    Product p =new Product();
+    System.out.println(p.product(2,3));
+    System.out.println(p.product(2,3,4));
+    System.out.println(p.product(3.45,44.4));
+  }
 }
