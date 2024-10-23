052ea3798d78e1245cdc6c15d7bada67d1015e8f
sarfudeen727262 <ahmad.sarfudeen@gmail.com>
2024-10-23 23:18:56 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,26 @@
 package com.driver;
-public class Product{
-   //fsdgf
-  public int product(int x, int y) {
-    return x*y;
-  }
-  public int product(int x, int y, int z) {
-    return x*y*z;
-  }
-  public double product(double x, double y) {
-    return x*y;
-  }
-public class Main{
- public static void main (String[] args){
-    Product p =new Product();
-    System.out.println(p.product(2,3));
-    System.out.println(p.product(2,3,4));
-    System.out.println(p.product(3.45,44.4));
-  }
-}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println( p.product(10, 3));
+        System.out.println(p.product(10, 3, 7));
+        System.out.println(p.product(10.4, 8.4));
+
+    }
+
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+    }
 }
